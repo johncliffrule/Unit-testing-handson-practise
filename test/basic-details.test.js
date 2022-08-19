1d4ea624af1c08fcc8dd27083b19feff105ee678
John Clifford <johncliffrule@gmail.com>
2022-08-19 14:13:47 +0530

    Second commit

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -1,20 +1,29 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
+import sinon, { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails';
+import { BasicDetails } from '../src/LoanBasicDetails/BasicDetails';
 
 describe('Basic details', () => {
   it('renders the basic details', async () => {
     const el = await fixture(html`<basic-details></basic-details>`);
     expect(el).to.be.accessible();
-  })
+  });
 
-  it('calls _captureDetails when submit/next btn is clicked', async () => {
-    const el = await fixture(html`<basic-details></basic-details>`);
-    const myFunctionStub = stub(el, '_captureDetails');
-    el.requestUpdate();
-    await el.updateComplete;
-   
-    el.shadowRoot.getElementById('lion-submit-btn').click();
-    expect(myFunctionStub).to.have.calledOnce();
-  })
+  var basicDetails;
+
+  beforeEach(() => {
+    basicDetails = new BasicDetails();
+  });
+
+  it('calls _captureDetails when submit/next btn is clicked', async() => {
+    setTimeout( async() => {
+      const el = await fixture(html`<basic-details></basic-details>`);      
+      const spy = sinon.spy(basicDetails._captureDetails);
+      el.requestUpdate();
+      await el.updateComplete;
+    
+      el.shadowRoot.getElementById('lion-submit-btn').click;      
+      expect(spy.called).to.be.true;
+    }, 2000);
+  });
 });
--- test/headers.test.js
+++ test/headers.test.js
@@ -1,10 +1,20 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
+import sinon, { stub } from 'sinon';
 import '../src/header/Header';
+import { Header } from '../src/header/Header';
 
 describe('loan-header', () => {
   it('loads the header fine', async ()=> {
     const el = await fixture(html`<loan-header></loan-header>`);
     expect(el).to.be.accessible();
+  });
+
+  it('calls locale method when language is clicked', async () => {
+    const el = await fixture(html`<loan-header></loan-header>`);
+    const header = new Header();    
+
+    el.shadowRoot.querySelector('button').click();
+    const spy = sinon.spy(header, "localeChanged");
+    // expect(spy.called).to.be.true();
   });
 });
